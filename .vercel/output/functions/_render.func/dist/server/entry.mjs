2b8470d0664e582218cf0e2496d7846aed91ac5f
Luisirk <luisirk.montoto@gmail.com>
2025-02-28 12:39:37 +0100

    global.css changes

--- .vercel/output/functions/_render.func/dist/server/entry.mjs
+++ .vercel/output/functions/_render.func/dist/server/entry.mjs
@@ -1,16 +1,16 @@
 import { renderers } from './renderers.mjs';
-import { c as createExports } from './chunks/entrypoint_CTlI_5GG.mjs';
-import { manifest } from './manifest_RDTOI6dW.mjs';
+import { c as createExports } from './chunks/entrypoint_DxOaqgbO.mjs';
+import { manifest } from './manifest_UYiDWPvs.mjs';
 
 const serverIslandMap = new Map([
-	['BuyButton', () => import('./chunks/BuyButton_DOl8p63y.mjs')],
+	['BuyButton', () => import('./chunks/BuyButton_Bpp5nhs2.mjs')],
 ]);;
 
 const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/libro/_id_.astro.mjs');
 const _page2 = () => import('./pages/index.astro.mjs');
 const pageMap = new Map([
-    ["node_modules/.pnpm/astro@5.3.1_jiti@2.4.2_ligh_0071d12fd0f09ef4854b0bd69339054b/node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
     ["src/pages/libro/[id].astro", _page1],
     ["src/pages/index.astro", _page2]
 ]);
@@ -22,7 +22,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "83deff35-910d-4a42-9ecb-a68dd1ae27a7",
+    "middlewareSecret": "19e4115d-cb52-4c89-9151-01f4cb81a3d4",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
